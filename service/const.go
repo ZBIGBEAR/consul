0b54b296c471238fbb754f4cdd408e07b46f3b70
664141154@qq.com <6641441154@qq.com>
2021-03-29 00:14:25 +0800

    'init'

---
+++ service/const.go
@@ -0,0 +1,7 @@
+package main
+
+const (
+	ServerAddr = "192.167.1.1:8500"
+	LocalIP = "localhost"
+	LocalPort = 123
+)
---
+++ service/register.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	consulapi "github.com/hashicorp/consul/api"
+)
+
+var (
+	consulClient consulapi.
+)
+
+func RegisterConsul() {
+
+}
+
+func
